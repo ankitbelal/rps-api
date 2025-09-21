65504e2f6f63c0ca2ab53459e7d0a50057bbd6f0
Ankit Belal <aankitbelal@gmail.com>
2025-09-21 23:11:56 +0545

    added typeorm and env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule,ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [],
