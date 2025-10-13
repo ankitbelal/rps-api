3f984ffb9b66a836de8d1a8583a1ae8adffd0991
Ankit Belal <aankitbelal@gmail.com>
2025-10-13 21:29:35 +0530

    set api prefix for all api routes except root

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { RequestMethod, ValidationPipe } from '@nestjs/common';
 import { JwtGuard } from './auth/jwt/jwt.guard';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api',{
+    exclude:[{path:'/', method:RequestMethod.ALL}],
+  });
+
   const reflector=app.get(Reflector);
   app.useGlobalGuards(new JwtGuard(app.get(JwtService),app.get(ConfigService),reflector));
   app.useGlobalPipes(new ValidationPipe({
