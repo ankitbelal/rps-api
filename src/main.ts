539e03fb3898d60809db72c10c126bbd96e0b22d
Ankit Belal <aankitbelal@gmail.com>
2025-10-21 10:00:54 +0530

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,16 +7,24 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api',{
-    exclude:[{path:'/', method:RequestMethod.ALL}],
+  app.setGlobalPrefix('api', {
+    exclude: [{ path: '/', method: RequestMethod.ALL }],
   });
 
-  const reflector=app.get(Reflector);
-  app.useGlobalGuards(new JwtGuard(app.get(JwtService),app.get(ConfigService),reflector));
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist:true,
-    transform:true
-  }))
+  const reflector = app.get(Reflector);
+  app.useGlobalGuards(
+    new JwtGuard(app.get(JwtService), app.get(ConfigService), reflector),
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+  app.enableCors({
+    origin: ['http://localhost:5173', 'https://ui-rps.vercel.app'],
+    Credential:true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
